8a343fc9849f6f21da53fd7f03bb3c109a073d66
anjalidhanjode28 <anjalidhanjode28@gmail.com>
2022-12-22 18:16:15 +0530

    teacher page

---
+++ client/pages/teachers/Model.js
@@ -0,0 +1,52 @@
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+    Button,
+    Text
+  } from '@chakra-ui/react';
+  import React from 'react';
+import { useDisclosure } from '@chakra-ui/react'
+  function Model() {
+    const OverlayOne = () => (
+      <ModalOverlay
+        bg='blackAlpha.300'
+        backdropFilter='blur(10px) hue-rotate(90deg)'
+      />
+    )
+  
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    const [overlay, setOverlay] = React.useState(<OverlayOne />)
+  
+    return (
+      <>
+        <Button
+          onClick={() => {
+            setOverlay(<OverlayOne />)
+            onOpen()
+          }}
+        >
+         Message
+        </Button>
+   
+        <Modal isCentered isOpen={isOpen} onClose={onClose}>
+          {overlay}
+          <ModalContent>
+            <ModalHeader>Modal Title</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              <Text>Custom backdrop filters!</Text>
+            </ModalBody>
+            <ModalFooter>
+              <Button onClick={onClose}>Close</Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+      </>
+    )
+  }
+  export default Model;
