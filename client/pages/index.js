c7f6169fe133176223f7d50704fb51eb9864589b
Ashutosh Sharma <104469510+mr-ashu@users.noreply.github.com>
2022-12-22 21:28:29 +0530

    some change

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,6 +1,6 @@
 
 import { Box, useColorModeValue } from "@chakra-ui/react"
-import { dividerClasses } from "@mui/material"
+ 
 import { Scroll } from "../components/HomeElement/Scroll"
 import { Top } from "../components/HomeElement/Top"
 import style from "../styles/Home.module.css"
@@ -8,14 +8,14 @@ export default function Home() {
 
   return (
 
-    <din>
+    <div>
       <Top />
       <Box bg={useColorModeValue("gray.100", "gray.900")} className={style.homebox}  >
 
         <Scroll />
 
       </Box>
-    </din>
+    </div>
 
 
   )
