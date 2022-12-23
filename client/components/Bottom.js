53f639e981dee99b0fa14d0d9064e8de1093635e
Ashutosh Sharma <104469510+mr-ashu@users.noreply.github.com>
2022-12-23 05:54:44 +0530

    cart and some-change

--- client/components/Bottom.js
+++ client/components/Bottom.js
@@ -1,11 +1,10 @@
 import { AddIcon } from "@chakra-ui/icons";
-import { Box, Button, Flex, useColorModeValue } from "@chakra-ui/react";
+import { Box, Button, Flex, Text, useColorModeValue } from "@chakra-ui/react";
 import * as React from "react";
- 
+import Link from 'next/link'
 import style from "./Navbar.module.css"
  
-
-//   <AddIcon />
+ 
  
 
 export default function Bottom() {
@@ -14,17 +13,34 @@ export default function Bottom() {
   return (
     <div className={style.bottom}>
      
-     <Box bg={useColorModeValue('	rgb(95, 68, 68,.6)', 'gray.900')} width="60%" borderRadius="10px" margin="auto" px={4}>
-        <Flex h={16} alignItems={'center'} justifyContent={'center'}>
-    
+     <Box bg={useColorModeValue('	rgb(95, 68, 68,.6)', 'gray.900')} width="50%" borderRadius="10px" margin="auto" px={4}>
+        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+        <Button
+              variant={'solid'}
+              colorScheme={'teal'}
+              size={'sm'}
+              mr={4}
+               >
+              <Link href="/teachers">TEACHERS</Link>
+            </Button>
+            <Button
+              variant={'solid'}
+              colorScheme={'red'}
+              size={'sm'}
+              mr={4}
+               >
+             <AddIcon />
+            </Button>
+
             <Button
               variant={'solid'}
               colorScheme={'teal'}
               size={'sm'}
               mr={4}
                >
-             <AddIcon />
+            <Link href="/videos"> VIDEO</Link>
             </Button>
+
           
           </Flex>
 
